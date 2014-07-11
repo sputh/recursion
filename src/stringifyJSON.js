ae48ff256d09ee0f36d04e1eba4887cf73edead9
sputh <samantha.puth@gmail.com>
2014-07-11 05:39:35 +0000

    First attempt to stringify for num,, obj, array, and boolean

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,34 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+
+//First thing JSON.stringify() does is convert a value to JSON
 var stringifyJSON = function(obj) {
   // your code goes here
+  /*var string;
+  if(obj.typeof() == 'num') {
+      string = obj;
+  } else if(obj.typeof() == 'obj') {
+      for(var key in obj) {
+          string += key + " " + obj[key];
+      }
+  } else if(obj.typeof() == 'array') {
+      for(var i = 0; i < obj.length; i++) {
+          if(i != obj.length -1) {
+              string += obj[i] + " ";
+          } else {
+              string += obj[i]
+          }
+      }
+  } 
+  else if(obj.typeof() == 'boolean') {
+      if(obj == 'true') {
+          return string = "true";
+      } else {
+          return string = "false";
+      }
+  }
+  return string;*/
+  return obj.stringifyJSON();
+
 };
